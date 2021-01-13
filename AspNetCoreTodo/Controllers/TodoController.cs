44b2e33d26e850be19a907d835ab7aeab66c9bf5
Abram Pithey <abramepithey@gmail.com>
2021-01-13 12:30:23 -0700

    Adds TodoController

---
+++ AspNetCoreTodo/Controllers/TodoController.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AspNetCoreTodo.Controllers
+{
+    public class TodoController : Controller
+    {
+        public IActionResult Index()
+        {
+            // get to-do items from database
+
+            // put items into a model
+
+            // render view using the model
+        }
+    }
+}
