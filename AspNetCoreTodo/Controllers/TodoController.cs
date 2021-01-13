cc822c5dfc62e97fa1177c00c1884a678283d2f2
Abram Pithey <abramepithey@gmail.com>
2021-01-13 12:55:04 -0700

    Adds dependency injection and fills Index method

--- AspNetCoreTodo/Controllers/TodoController.cs
+++ AspNetCoreTodo/Controllers/TodoController.cs
@@ -2,19 +2,31 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AspNetCoreTodo.Models;
+using AspNetCoreTodo.Services;
 using Microsoft.AspNetCore.Mvc;
 
 namespace AspNetCoreTodo.Controllers
 {
     public class TodoController : Controller
     {
-        public IActionResult Index()
+        private readonly ITodoItemService _todoItemService;
+
+        public TodoController(ITodoItemService todoItemService)
         {
-            // get to-do items from database
+            _todoItemService = todoItemService;
+        }
 
-            // put items into a model
+        public async Task<IActionResult> Index()
+        {
+            var items = await _todoItemService.GetIncompleteItemsAsync();
 
-            // render view using the model
+            var model = new TodoViewModel()
+            {
+                Items = items
+            };
+
+            return View(model);
         }
     }
 }
