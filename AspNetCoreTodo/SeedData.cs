88e89711553d1728778fb06c5221b56d3323ffa2
Abram Pithey <abramepithey@gmail.com>
2021-01-18 21:09:33 -0700

    Adds test admin class to ensure there is always an admin account created

---
+++ AspNetCoreTodo/SeedData.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Threading.Tasks;
+using AspNetCoreTodo.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace AspNetCoreTodo
+{
+    public static class SeedData
+    {
+        public static async Task InitializeAsync(IServiceProvider services)
+        {
+            var roleManager = services.GetRequiredService<RoleManager<IdentityRole>>();
+            await EnsureRolesAsync(roleManager);
+
+            var userManager = services.GetRequiredService<UserManager<ApplicationUser>>();
+
+            await EnsureTestAdminAsync(userManager);
+        }
+
+        private static async Task EnsureRolesAsync(RoleManager<IdentityRole> roleManager)
+        {
+            var alreadyExists = await roleManager.RoleExistsAsync(Constants.AdministratorRole);
+            if (alreadyExists)
+                return;
+            
+            await roleManager.CreateAsync(new IdentityRole(Constants.AdministratorRole));
+        }
+    }
+}
